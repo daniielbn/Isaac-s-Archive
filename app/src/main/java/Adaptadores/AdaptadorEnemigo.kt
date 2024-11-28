1ee7de63e23052d2b7005cf5df77cdcc10663bd7
daniielbn <danielbritonegrin@gmail.com>
2024-11-28 21:41:05 +0000

    Implementada las funcionalidades para los enemigos. Añadida lista con todos los enemigos, y añadido el vista para cada enemigo. También, se han implementado los comentarios para cada enemigo

--- app/src/main/java/Adaptadores/AdaptadorEnemigo.kt
+++ app/src/main/java/Adaptadores/AdaptadorEnemigo.kt
@@ -1,18 +1,27 @@
-package com.example.isaacsarchive
+package Adaptadores
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import Clases.AdminSQLiteOpenHelper
+import Clases.Enemigo
+import androidx.core.content.ContextCompat.startActivity
+import com.example.isaacsarchive.ItemEnemigoActivity
+import com.example.isaacsarchive.PrincipalObjetosActivity
+import com.example.isaacsarchive.R
 
 class AdaptadorEnemigo(
-    var listaEnemigos: ArrayList<Enemigo>
+    var listaEnemigos: ArrayList<Enemigo>,
+    var usuario: String
 ): RecyclerView.Adapter<AdaptadorEnemigo.ViewHolderEnemigo>() {
+    private lateinit var ventanaItemEnemigo: Intent
 
     class ViewHolderEnemigo(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val nombre = itemView.findViewById<TextView>(R.id.twNombre)
+        val nombre = itemView.findViewById<TextView>(R.id.twUsuarioComentario)
         val img = itemView.findViewById<ImageView>(R.id.imgLogoLista)
         val descripcion = itemView.findViewById<TextView>(R.id.twDescripcion)
         val cbDesbloqueado = itemView.findViewById<TextView>(R.id.cbDesbloqueado)
@@ -39,8 +48,15 @@ class AdaptadorEnemigo(
         val nombreImagen = obtenerRuta(enemigo.nombre)
         val idImagen = holder.img.context.resources.getIdentifier(nombreImagen, "drawable", holder.img.context.packageName)
         holder.img.setImageResource(idImagen)
-    }
 
+        holder.img.setOnClickListener {
+            ventanaItemEnemigo = Intent(holder.img.context, ItemEnemigoActivity::class.java)
+            ventanaItemEnemigo.putExtra("usuario", usuario)
+            ventanaItemEnemigo.putExtra("enemigo", enemigo)
+            holder.img.context.startActivity(ventanaItemEnemigo)
+        }
+
+    }
 
     fun obtenerRuta(nombre: String): String {
         var nombreFormateado = nombre.toLowerCase().replace(" ", "_").replace("'", "").replace(".", "")

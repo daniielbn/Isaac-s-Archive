685c5329df8aea27e9964ac2ca8898c793d0197a
daniielbn <danielbritonegrin@gmail.com>
2024-12-03 18:54:16 +0000

    Se han eliminado importaciones innecesarias.

--- app/src/main/java/com/example/isaacsarchive/PerfilActivity.kt
+++ app/src/main/java/com/example/isaacsarchive/PerfilActivity.kt
@@ -3,8 +3,6 @@ package com.example.isaacsarchive
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class PerfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

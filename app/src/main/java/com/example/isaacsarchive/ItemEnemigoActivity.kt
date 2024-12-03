100473b9937141c7e8468b14f0a6163a8c4d8e20
daniielbn <danielbritonegrin@gmail.com>
2024-12-03 18:53:34 +0000

    Simplificamos el código con menos accesos a la base de datos. Además, se han eliminado importaciones innecesarias.

--- app/src/main/java/com/example/isaacsarchive/ItemEnemigoActivity.kt
+++ app/src/main/java/com/example/isaacsarchive/ItemEnemigoActivity.kt
@@ -4,37 +4,38 @@ import Adaptadores.AdaptadorComentario
 import Clases.AdminSQLiteOpenHelper
 import Clases.Comentario
 import Clases.Enemigo
-import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.isaacsarchive.databinding.ActivityItemEnemigoBinding
-import com.example.isaacsarchive.databinding.ActivityItemObjetoBinding
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class ItemEnemigoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityItemEnemigoBinding
     private lateinit var adaptadorComentario: AdaptadorComentario
     private lateinit var ventanaObjeto: Intent
+    private lateinit var db: AdminSQLiteOpenHelper
 
     private lateinit var twUsuario: TextView
     private lateinit var twTitulo: TextView
     private lateinit var twDescripcion: TextView
     private lateinit var twTipo: TextView
-    private lateinit var cbDesbloqueado: TextView
+    private lateinit var cbDesbloqueado: CheckBox
     private lateinit var imgEnemigo: ImageView
     private lateinit var imgVolver: ImageView
     private lateinit var etComentario: EditText
+    private lateinit var buttonComentar: Button
 
     private lateinit var usuario: String
     private lateinit var enemigo: Enemigo
@@ -44,12 +45,12 @@ class ItemEnemigoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityItemEnemigoBinding.inflate(layoutInflater)
         usuario = intent.getStringExtra("usuario").toString()
-        adaptadorComentario = AdaptadorComentario(listaComentarios, usuario)
-        setContentView(binding.root)
-        setupRecyclerView()
-
-        usuario = intent.getStringExtra("usuario").toString()
         enemigo = intent.getSerializableExtra("enemigo") as Enemigo
+        setContentView(binding.root)
+        db = AdminSQLiteOpenHelper(this, "IsaacsArchive", null, 7)
+        listaComentarios = db.obtenerComentariosEnemigo(enemigo)
+        adaptadorComentario = AdaptadorComentario(listaComentarios, usuario)
+        setupRecyclerView()
 
         twUsuario = findViewById(R.id.twPerfil)
         twTitulo = findViewById(R.id.twTituloEnemigo)
@@ -59,13 +60,23 @@ class ItemEnemigoActivity : AppCompatActivity() {
         imgEnemigo = findViewById(R.id.imgEnemigo)
         imgVolver = findViewById(R.id.imgVolver)
         etComentario = findViewById(R.id.etComentario)
+        buttonComentar = findViewById(R.id.buttonComentario)
 
         twUsuario.text = usuario
-
-        ventanaObjeto = Intent(this, PrincipalEnemigosActivity::class.java)
-
         llenarEnemigo()
-        llenarComentarios()
+
+        ventanaObjeto = Intent(this, PrincipalObjetosActivity::class.java)
+
+        cbDesbloqueado.isChecked = db.comprobarDesbloqueoEnemigo(enemigo, usuario)
+        cbDesbloqueado.setOnCheckedChangeListener() { buttonView, isChecked ->
+            if (isChecked) {
+                enemigo.desbloqueado = true
+                actualizarEstadoDesbloqueado(enemigo, 0)
+            } else {
+                enemigo.desbloqueado = false
+                actualizarEstadoDesbloqueado(enemigo, 1)
+            }
+        }
     }
 
     fun abrirObjetos(v: View?) {
@@ -78,111 +89,43 @@ class ItemEnemigoActivity : AppCompatActivity() {
     }
 
     fun anadirComentario(v: View?) {
-        val comentario = etComentario.text.toString()
-        val idUsuario = consultarIdUsuario(usuario)
-        val idEnemigo = consultarIdEnemigo(enemigo.nombre)
-
-        if (comentario.isNotEmpty()) {
-            enviarComentario(comentario, idUsuario, idEnemigo)
+        if (etComentario.text.toString().isNotEmpty()) {
+            enviarComentario(
+                etComentario.text.toString(),
+                db.consultarUsuario(usuario),
+                db.consultarEnemigo(enemigo.nombre)
+            )
             etComentario.text.clear()
         } else {
-            Toast.makeText(this, "El comentario no puede estar vacío.", Toast.LENGTH_SHORT)
+            Toast.makeText(this, "El comentario no puede estar vacío", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun enviarComentario(comentario: String, idUsuario: Int, idEnemigo: Int) {
-
-        val admin = AdminSQLiteOpenHelper(this, "IsaacsArchive1.0", null, 1)
-        val bd = admin.writableDatabase
-
-        try {
-            val valores = ContentValues().apply {
-                put("id_usuario", idUsuario)
-                put("id_enemigo", idEnemigo)
-                put("comentario", comentario)
-                put("fecha", System.currentTimeMillis())
-            }
-            val id = bd.insert("Comentarios", null, valores)
-            if (id == -1L) {
-                Toast.makeText(this, "Error al agregar el comentario", Toast.LENGTH_SHORT).show()
+    private fun actualizarEstadoDesbloqueado(enemigo: Enemigo, opcion: Int) {
+        if (opcion == 0) {
+            if (db.actualizarDesbloqueadoEnemigo(enemigo, usuario)) {
+                Toast.makeText(this, "Se actualizó el estado de desbloqueado.\nEl enemigo ahora está desbloqueado.", Toast.LENGTH_SHORT).show()
             } else {
-                val comentario = Comentario(idUsuario, idEnemigo, null, comentario, System.currentTimeMillis())
-                listaComentarios.add(comentario)
-
-                adaptadorComentario.notifyItemInserted(listaComentarios.size - 1)
-                binding.rvListaComentarios.scrollToPosition(listaComentarios.size - 1)
-
-                Toast.makeText(this, "Comentario agregado con éxito", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Error al actualizar el estado de desbloqueado", Toast.LENGTH_SHORT).show()
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Toast.makeText(this, "Ocurrió un error al agregar el comentario", Toast.LENGTH_SHORT).show()
-        } finally {
-            bd.close()
-        }
-    }
-
-    private fun consultarIdEnemigo(nombre: String): Int {
-        val admin = AdminSQLiteOpenHelper(this, "IsaacsArchive1.0", null, 1)
-        val db = admin.writableDatabase
-        try {
-            val cursor = db.rawQuery("SELECT id FROM Enemigos WHERE nombre = ?", arrayOf(nombre))
-            if (cursor.moveToFirst()) {
-                return cursor.getInt(0)
-            }
-            cursor.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            db.close()
-        }
-        return -1
-    }
-
-    private fun consultarIdUsuario(usuario: String): Int {
-        val admin = AdminSQLiteOpenHelper(this, "IsaacsArchive1.0", null, 1)
-        val db = admin.writableDatabase
-        try {
-             val cursor = db.rawQuery("SELECT id FROM Usuarios WHERE usuario = ?", arrayOf(usuario))
-            if (cursor.moveToFirst()) {
-                return cursor.getInt(0)
-            }
-            cursor.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            db.close()
-        }
-        return -1
-    }
-
-    private fun llenarComentarios() {
-        val admin = AdminSQLiteOpenHelper(this, "IsaacsArchive1.0", null, 1)
-        val db = admin.readableDatabase
-        try {
-            if (consultarIdEnemigo(enemigo.nombre) != null) {
-                val cursor = db.rawQuery("SELECT id_usuario, id_objeto, id_enemigo, comentario, fecha FROM Comentarios WHERE id_enemigo = ?", arrayOf(consultarIdEnemigo(enemigo.nombre).toString()))
-                if (cursor.moveToFirst()) {
-                    do {
-                        val id_usuario = cursor.getInt(0)
-                        val id_objeto = if (!cursor.isNull(1)) cursor.getInt(1) else null
-                        val id_enemigo = cursor.getInt(2)
-                        val comentario = cursor.getString(3)
-                        val fecha = cursor.getLong(4)
-                        val comentarioObjeto = Comentario(id_usuario, id_objeto, id_enemigo, comentario, fecha)
-                        listaComentarios.add(comentarioObjeto)
-                    } while (cursor.moveToNext())
-                }
-                cursor.close()
+        } else {
+            if (db.eliminarDesbloqueoEnemigo(enemigo, usuario)) {
+                Toast.makeText(this, "Se actualizó el estado de desbloqueado.\nEl enemigo ahora está bloqueado.", Toast.LENGTH_SHORT).show()
             } else {
-                Log.e("Error", "No se ha encontrado el objeto")
+                Toast.makeText(this, "Error al actualizar el estado de desbloqueado", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
 
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.e("Error", "Error al cargar los comentarios")
-        } finally {
-            db.close()
+    private fun enviarComentario(nuevoComentario: String, idUsuario: Int, idEnemigo: Int) {
+        val comentario = Comentario(idUsuario, null, idEnemigo, nuevoComentario, getFechaActual())
+        if (db.anadirComentarioEnemigo(comentario)) {
+            listaComentarios.add(comentario)
+            adaptadorComentario.notifyItemInserted(listaComentarios.size - 1)
+            binding.rvListaComentarios.scrollToPosition(listaComentarios.size - 1)
+            Toast.makeText(this, "Comentario agregado con éxito", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Error al agregar el comentario", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -190,6 +133,7 @@ class ItemEnemigoActivity : AppCompatActivity() {
         twTitulo.text = enemigo.nombre
         twDescripcion.text = enemigo.descripcion
         cbDesbloqueado.isSelected = enemigo.desbloqueado
+        twTipo.text = enemigo.tipo
 
         val nombreImagen = obtenerRuta(enemigo.nombre)
         val idImagen = imgEnemigo.context.resources.getIdentifier(nombreImagen, "drawable", imgEnemigo.context.packageName)
@@ -197,11 +141,19 @@ class ItemEnemigoActivity : AppCompatActivity() {
     }
 
     private fun obtenerRuta(nombre: String): String {
-        return nombre.lowercase().replace("-", "").replace("'", "")
+        if (nombre.lowercase().equals("gemini")) {
+            return "gemini_enemigo"
+        }
+        return nombre.lowercase().replace(" ", "_").replace("'", "").replace(".", "")
     }
 
     private fun setupRecyclerView() {
         binding.rvListaComentarios.layoutManager = LinearLayoutManager(this)
         binding.rvListaComentarios.adapter = adaptadorComentario
+    }
+
+    private fun getFechaActual(): String {
+        val formato =  DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss", Locale.getDefault())
+        return LocalDateTime.now().format(formato)
     }
 }
--- app/src/main/java/com/example/isaacsarchive/ItemObjetoActivity.kt
+++ app/src/main/java/com/example/isaacsarchive/ItemObjetoActivity.kt
@@ -4,10 +4,8 @@ import Adaptadores.AdaptadorComentario
 import Clases.AdminSQLiteOpenHelper
 import Clases.Comentario
 import Clases.Objeto
-import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
@@ -17,15 +15,18 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.isaacsarchive.databinding.ActivityItemObjetoBinding
-import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 class ItemObjetoActivity : AppCompatActivity() {
+    private var primero = true
     private lateinit var binding: ActivityItemObjetoBinding
     private lateinit var adaptadorComentario: AdaptadorComentario
     private lateinit var ventanaEnemigos: Intent
+    private lateinit var db: AdminSQLiteOpenHelper
+
 
     private lateinit var twUsuario: TextView
     private lateinit var twTitulo: TextView
@@ -35,6 +36,7 @@ class ItemObjetoActivity : AppCompatActivity() {
     private lateinit var imgObjeto: ImageView
     private lateinit var imgVolver: ImageView
     private lateinit var etComentario: EditText
+    private lateinit var buttonComentar: Button
 
     private lateinit var usuario: String
     private lateinit var objeto: Objeto
@@ -44,12 +46,15 @@ class ItemObjetoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityItemObjetoBinding.inflate(layoutInflater)
         usuario = intent.getStringExtra("usuario").toString()
-        adaptadorComentario = AdaptadorComentario(listaComentarios, usuario)
+        objeto = intent.getSerializableExtra("objeto") as Objeto
         setContentView(binding.root)
+        db = AdminSQLiteOpenHelper(this, "IsaacsArchive", null, 7)
+        listaComentarios = db.obtenerComentariosObjeto(objeto)
+        adaptadorComentario = AdaptadorComentario(listaComentarios, usuario)
         setupRecyclerView()
 
-        twUsuario = findViewById(R.id.twPerfil)
 
+        twUsuario = findViewById(R.id.twPerfil)
         twTitulo = findViewById(R.id.twTituloObjeto)
         twDescripcion = findViewById(R.id.twDescripcionObjeto)
         twTipo = findViewById(R.id.twTipoObjeto)
@@ -57,15 +62,23 @@ class ItemObjetoActivity : AppCompatActivity() {
         imgObjeto = findViewById(R.id.imgObjeto)
         imgVolver = findViewById(R.id.imgVolver)
         etComentario = findViewById(R.id.etComentario)
+        buttonComentar = findViewById(R.id.buttonComentario)
 
         twUsuario.text = usuario
-
-        objeto = intent.getSerializableExtra("objeto") as Objeto
+        llenarObjeto()
 
         ventanaEnemigos = Intent(this, PrincipalEnemigosActivity::class.java)
 
-        llenarObjeto()
-        llenarComentarios()
+        cbDesbloqueado.isChecked = db.comprobarDesbloqueoObjeto(objeto, usuario)
+        cbDesbloqueado.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                objeto.desbloqueado = true
+                actualizarEstadoDesbloqueado(objeto, 0)
+            } else {
+                objeto.desbloqueado = false
+                actualizarEstadoDesbloqueado(objeto, 1)
+            }
+        }
     }
 
     fun abrirEnemigos(v: View?) {
@@ -78,47 +91,62 @@ class ItemObjetoActivity : AppCompatActivity() {
     }
 
     fun anadirComentario(v: View?) {
-        val comentario = etComentario.text.toString()
-        val idUsuario = consultarIdUsuario(usuario)
-        val idObjeto = consultarIdObjeto(objeto.nombre)
-
-        if (comentario.isNotEmpty()) {
-            enviarComentario(comentario, idUsuario, idObjeto)
+        if (etComentario.text.toString().isNotEmpty()) {
+            enviarComentario(
+                etComentario.text.toString(),
+                db.consultarUsuario(usuario),
+                db.consultarObjeto(objeto.nombre)
+            )
             etComentario.text.clear()
         } else {
             Toast.makeText(this, "El comentario no puede estar vacío", Toast.LENGTH_SHORT).show()
         }
     }
 
-    fun enviarComentario(nuevoComentario: String, idUsuario: Int, idObjeto: Int) {
-        val admin = AdminSQLiteOpenHelper(this, "IsaacsArchive1.0", null, 1)
-        val bd = admin.writableDatabase
-
-        try {
-            val valores = ContentValues().apply {
-                put("id_usuario", idUsuario)
-                put("id_objeto", idObjeto)
-                put("comentario", nuevoComentario)
-                put("fecha", System.currentTimeMillis())
-            }
-            val id = bd.insert("Comentarios", null, valores)
-            if (id == -1L) {
-                Toast.makeText(this, "Error al agregar el comentario", Toast.LENGTH_SHORT).show()
+    private fun actualizarEstadoDesbloqueado(objeto: Objeto, opcion: Int) {
+        if (opcion == 0) {
+            if (db.actualizarDesbloqueadoObjeto(objeto, usuario)) {
+                Toast.makeText(this, "Se actualizó el estado de desbloqueado.\n El objeto ahora está desbloqueado.", Toast.LENGTH_SHORT).show()
             } else {
-                val comentario = Comentario(idUsuario, idObjeto, null, nuevoComentario, System.currentTimeMillis())
-                listaComentarios.add(comentario)
-
-                adaptadorComentario.notifyItemInserted(listaComentarios.size - 1)
-                binding.rvListaComentarios.scrollToPosition(listaComentarios.size - 1)
-
-                Toast.makeText(this, "Comentario agregado con éxito", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Error al actualizar el estado de desbloqueado", Toast.LENGTH_SHORT).show()
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Toast.makeText(this, "Ocurrió un error al agregar el comentario", Toast.LENGTH_SHORT).show()
-        } finally {
-            bd.close()
+        } else {
+            if (db.eliminarDesbloqueoObjeto(objeto, usuario)) {
+                Toast.makeText(this, "Se actualizó el estado de desbloqueado.\nEl objeto ahora esta bloqueado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Error al actualizar el estado de desbloqueado", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    fun enviarComentario(nuevoComentario: String, idUsuario: Int, idObjeto: Int) {
+        val comentario = Comentario(idUsuario, idObjeto, null, nuevoComentario, getFechaActual())
+        if (db.anadirComentarioObjeto(comentario)) {
+            listaComentarios.add(comentario)
+            adaptadorComentario.notifyItemInserted(listaComentarios.size - 1)
+            binding.rvListaComentarios.scrollToPosition(listaComentarios.size - 1)
+            Toast.makeText(this, "Comentario agregado con éxito", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Error al agregar el comentario", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun llenarObjeto() {
+        twTitulo.text = objeto.nombre
+        twDescripcion.text = objeto.descripcion
+        cbDesbloqueado.isSelected = objeto.desbloqueado
+        twTipo.text = objeto.rareza
+
+        val nombreImagen = obtenerRuta(objeto.nombre)
+        val idImagen = imgObjeto.context.resources.getIdentifier(nombreImagen, "drawable", imgObjeto.context.packageName)
+        imgObjeto.setImageResource(idImagen)
+    }
+
+    private fun obtenerRuta(nombre: String): String {
+        if (nombre.lowercase().equals("gemini")) {
+            return "gemini_objeto"
+        }
+        return nombre.lowercase().replace(" ", "_").replace("'", "").replace(".", "")
     }
 
     private fun setupRecyclerView() {
@@ -126,81 +154,8 @@ class ItemObjetoActivity : AppCompatActivity() {
         binding.rvListaComentarios.adapter = adaptadorComentario
     }
 
-    private fun llenarObjeto() {
-        twTitulo.text = objeto.nombre
-        twDescripcion.text = objeto.descripcion
-        cbDesbloqueado.isSelected = objeto.desbloqueado
-
-        val nombreImagen = obtenerRuta(objeto.nombre)
-        val idImagen = imgObjeto.context.resources.getIdentifier(nombreImagen, "drawable", imgObjeto.context.packageName)
-        imgObjeto.setImageResource(idImagen)
-    }
-
-    private fun llenarComentarios() {
-        val admin = AdminSQLiteOpenHelper(this, "IsaacsArchive1.0", null, 1)
-        val db = admin.readableDatabase
-        try {
-            if (consultarIdObjeto(objeto.nombre) != null) {
-                val cursor = db.rawQuery("SELECT id_usuario, id_objeto, id_enemigo, comentario, fecha FROM Comentarios WHERE id_objeto = ?", arrayOf(consultarIdObjeto(objeto.nombre).toString()))
-                if (cursor.moveToFirst()) {
-                    do {
-                        val id_usuario = cursor.getInt(0)
-                        val id_objeto = cursor.getInt(1)
-                        val id_enemigo = if (!cursor.isNull(2)) cursor.getInt(2) else null
-                        val comentario = cursor.getString(3)
-                        val fecha = cursor.getLong(4)
-                        val comentarioObjeto = Comentario(id_usuario, id_objeto, id_enemigo, comentario, fecha)
-                        listaComentarios.add(comentarioObjeto)
-                    } while (cursor.moveToNext())
-                }
-                cursor.close()
-            } else {
-               Log.e("Error", "No se ha encontrado el objeto")
-            }
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.e("Error", "Error al cargar los comentarios")
-        } finally {
-            db.close()
-        }
-    }
-
-    private fun obtenerRuta(nombre: String): String {
-        return nombre.toLowerCase().replace(" ", "_").replace("'", "")
-    }
-
-    private fun consultarIdUsuario(nombre: String): Int {
-        val admin = AdminSQLiteOpenHelper(this, "IsaacsArchive1.0", null, 1)
-        val db = admin.readableDatabase
-        try {
-            val cursor = db.rawQuery("SELECT id FROM Usuarios WHERE usuario = ?", arrayOf(nombre))
-            if (cursor.moveToFirst()) {
-                return cursor.getInt(0)
-            }
-            cursor.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            db.close()
-        }
-        return -1
-    }
-
-    private fun consultarIdObjeto(nombre: String): Int {
-        val admin = AdminSQLiteOpenHelper(this, "IsaacsArchive1.0", null, 1)
-        val db = admin.readableDatabase
-        try {
-            val cursor = db.rawQuery("SELECT id FROM Objetos WHERE nombre = ?", arrayOf(nombre))
-            if (cursor.moveToFirst()) {
-                return cursor.getInt(0)
-            }
-            cursor.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            db.close()
-        }
-        return -1
+    private fun getFechaActual(): String {
+        val formato =  DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss", Locale.getDefault())
+        return LocalDateTime.now().format(formato)
     }
 }

4a2029ca1f9341c63b29d8216103710eaab51a73
daniielbn <danielbritonegrin@gmail.com>
2024-12-15 15:13:27 +0000

    Modificada la clase creando un nuevo método para reiniciar la aplicación.

--- app/src/main/java/BaseActivity/BaseActivity.kt
+++ app/src/main/java/BaseActivity/BaseActivity.kt
@@ -1,7 +1,9 @@
 package BaseActivity
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import com.example.isaacsarchive.Principales.PrincipalObjetosActivity
 
 open class BaseActivity : AppCompatActivity() {
     override fun attachBaseContext(newBase: Context) {
@@ -15,5 +17,13 @@ open class BaseActivity : AppCompatActivity() {
         val configuracion = context.resources.configuration
         configuracion.fontScale = esclaa
         return context.createConfigurationContext(configuracion)
+    }
+
+    protected fun reinciarAplicacion() {
+        val intent = Intent(applicationContext, PrincipalObjetosActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(intent)
+        finish()
     }
 }
